db307dbfef276e01e7071ee0977fcd22e5955637
muhammedjepbarov <muhammedjepbarov@gmail.com>
2026-01-27 16:53:56 +0500

    done improvements

--- static/js/custom.js
+++ static/js/custom.js
@@ -93,8 +93,6 @@
 		loop: true,
 		margin: 20,
 		nav: false,
-		// autoplay: true,
-		// autoplayHoverPause: true,
 		dots: true,
 		responsive:{
 			0:{
@@ -136,15 +134,6 @@
 		}
 	})
 	
-	// Odometer JS
-	$('.odometer').appear(function(e) {
-		var odo = $(".odometer");
-		odo.each(function() {
-			var countNumber = $(this).attr("data-count");
-			$(this).html(countNumber);
-		});
-	});
-
 	// Tabs
 	$('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
 	$('.tab ul.tabs li').on('click', function (g) {
@@ -161,58 +150,6 @@
 	// WOW JS
 	new WOW().init();
 
-	// Subscribe form JS
-	$(".newsletter-form").validator().on("submit", function (event) {
-		if (event.isDefaultPrevented()) {
-			// handle the invalid form...
-			formErrorSub();
-			submitMSGSub(false, "Please enter your email correctly.");
-		} else {
-			// everything looks good!
-			event.preventDefault();
-		}
-	});
-	function callbackFunction (resp) {
-		if (resp.result === "success") {
-			formSuccessSub();
-		}
-		else {
-			formErrorSub();
-		}
-	}
-	function formSuccessSub(){
-		$(".newsletter-form")[0].reset();
-		submitMSGSub(true, "Thank you for subscribing!");
-		setTimeout(function() {
-			$("#validator-newsletter").addClass('hide');
-		}, 4000)
-	}
-	function formErrorSub(){
-		$(".newsletter-form").addClass("animated shake");
-		setTimeout(function() {
-			$(".newsletter-form").removeClass("animated shake");
-		}, 1000)
-	}
-	function submitMSGSub(valid, msg){
-		if(valid){
-			var msgClasses = "validation-success";
-		} else {
-			var msgClasses = "validation-danger";
-		}
-		$("#validator-newsletter").removeClass().addClass(msgClasses).text(msg);
-	}
-	
-	// AJAX MailChimp JS
-	$(".newsletter-form").ajaxChimp({
-		url: "https://EnvyTheme.us20.list-manage.com/subscribe/post?u=60e1ffe2e8a68ce1204cd39a5&amp;id=42d6d188d9", // Your url MailChimp
-		callback: callbackFunction
-	});
-
-	// Preloader JS
-	jQuery(window).on('load',function(){
-		jQuery(".preloader").fadeOut(500);
-	});
-
 	// Go to Top
 	$(window).on('scroll', function() {
         if ($(this).scrollTop() > 0) {
@@ -220,17 +157,9 @@
         } else {
             $('.go-top').removeClass('active');
         }
-	});	
-    $(function(){
-        $(window).on('scroll', function(){
-            var scrolled = $(window).scrollTop();
-            if (scrolled > 600) $('.go-top').addClass('active');
-            if (scrolled < 600) $('.go-top').removeClass('active');
-        });  
-        
-        $('.go-top').on('click', function() {
-            $("html, body").animate({ scrollTop: "0" },  500);
-        });
-    });
+	});
+	$('.go-top').on('click', function() {
+		$("html, body").animate({ scrollTop: "0" },  500);
+	});
 
 })(jQuery);
